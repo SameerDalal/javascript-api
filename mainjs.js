40abfe9688d73e0488d08e24c7024d6c47fbc384
SameerDalal <sameerdalal747@gmail.com>
2022-06-09 17:46:20 -0400

    init

---
+++ mainjs.js
@@ -0,0 +1,10 @@
+const url = undefined;
+        // create the url with the opening query and its componets such as the api key and endpoints
+
+        async function getData() {
+            const response = await fetch(url);
+            const data = await response.json();
+            // convertts the respose to json type
+            console.log(data)
+        }       
+    getData();
